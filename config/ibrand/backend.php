4030c24cf7e92329e6930fecc82270e29e7b0d19
lifeagin <yaochao133@163.com>
2018-11-13 12:10:32 +0800

    搭建开源2.x后台框架

---
+++ config/ibrand/backend.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of ibrand/backend.
+ *
+ * (c) iBrand <https://www.ibrand.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    /*
+    * Laravel-admin name.
+    */
+    'name' => 'iBrand 管理后台',
+
+    /*
+     * Logo in admin panel header.
+     */
+    'logo' => '<b>iBrand</b> 管理后台',
+
+    /*
+     * Mini-logo in admin panel header.
+     */
+    'logo-mini' => 'B',
+
+    /*
+     * Laravel-admin html title.
+     */
+    'title' => 'iBrand 管理后台',
+
+    'disks' => [
+        'admin' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/backend'),
+            'url' => env('APP_URL').'/storage/backend',
+            'visibility' => 'public',
+        ],
+    ],
+];
