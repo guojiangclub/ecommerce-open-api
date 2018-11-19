372d39fb90e306ce02f724268bcc3f14b1144354
zidaibudai <coolchen.hao@gmail.com>
2018-11-19 19:55:52 +0800

    update .env name.

--- github/component/Pay/src/config.php
+++ github/component/Pay/src/config.php
@@ -21,21 +21,21 @@ return [
         'alipay' => [
             'default' => [
                 // 支付宝分配的 APPID
-                'app_id' => env('ALI_APP_ID', ''),
+                'app_id' => env('ALI_PAYMENT_APP_ID', ''),
                 // 支付宝异步通知地址
                 'notify_url' => '',
                 // 支付成功后同步通知地址
                 'return_url' => '',
                 // 阿里公共密钥，验证签名时使用
-                'ali_public_key' => env('ALI_PUBLIC_KEY', ''),
+                'ali_public_key' => env('ALI_PAYMENT_PUBLIC_KEY', ''),
                 // 自己的私钥，签名时使用
-                'private_key' => env('ALI_PRIVATE_KEY', ''),
+                'private_key' => env('ALI_PAYMENT_PRIVATE_KEY', ''),
                 // optional，默认 warning；日志路径为：sys_get_temp_dir().'/logs/yansongda.pay.log'
                 'log' => [
                     'file' => storage_path('logs/alipay.log'),
                     //  'level' => 'debug'
-                      'type' => 'single', // optional, 可选 daily.
-                      'max_file' => 30,
+                    'type' => 'single', // optional, 可选 daily.
+                    'max_file' => 30,
                 ],
                 // optional，设置此参数，将进入沙箱模式
                 // 'mode' => 'dev',
@@ -45,17 +45,17 @@ return [
         'wechat' => [
             'default' => [
                 // 公众号 APPID
-                'app_id' => env('WECHAT_APP_ID', ''),
+                'app_id' => env('WECHAT_PAYMENT_APP_ID', ''),
                 // 小程序 APPID
-                'miniapp_id' => env('WECHAT_MINIAPP_ID', ''),
+                'miniapp_id' => env('WECHAT_PAYMENT_MINIAPP_ID', ''),
                 // APP 引用的 appid
-                'appid' => env('WECHAT_APPID', ''),
+                'appid' => env('WECHAT_PAYMENT_APPID', ''),
                 // 微信支付分配的微信商户号
-                'mch_id' => env('WECHAT_MCH_ID', ''),
+                'mch_id' => env('WECHAT_PAYMENT_MCH_ID', ''),
                 // 微信支付异步通知地址
-                'notify_url' => '',
+                'notify_url' => '/notify/wechat',
                 // 微信支付签名秘钥
-                'key' => env('WECHAT_KEY', ''),
+                'key' => env('WECHAT_PAYMENT_KEY', ''),
                 // 客户端证书路径，退款、红包等需要用到。请填写绝对路径，linux 请确保权限问题。pem 格式。
                 'cert_client' => '',
                 // 客户端秘钥路径，退款、红包等需要用到。请填写绝对路径，linux 请确保权限问题。pem 格式。
@@ -64,8 +64,8 @@ return [
                 'log' => [
                     'file' => storage_path('logs/wechat.log'),
                     //  'level' => 'debug'
-                      'type' => 'single', // optional, 可选 daily.
-                      'max_file' => 30,
+                    'type' => 'single', // optional, 可选 daily.
+                    'max_file' => 30,
                 ],
                 // optional
                 // 'dev' 时为沙箱模式
