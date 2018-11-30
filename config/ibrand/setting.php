4bb2a998aec7179b313bdd5066a67b733c26d15e
zidaibudai <coolchen.hao@gmail.com>
2018-11-30 14:33:20 +0800

    update migration files.

---
+++ config/ibrand/setting.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of ibrand/setting.
+ *
+ * (c) iBrand <https://www.ibrand.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    'table_name' => 'ibrand_system_settings',
+
+    'cache' => true,
+];
