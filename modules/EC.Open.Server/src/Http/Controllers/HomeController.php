3a577f99bc797d280e911a9f84812c3db1dff0a0
tangwei666 <18670055879@163.com>
2019-09-16 18:56:23 +0800

    /

--- modules/EC.Open.Server/src/Http/Controllers/HomeController.php
+++ modules/EC.Open.Server/src/Http/Controllers/HomeController.php
@@ -1,5 +1,4 @@
 ﻿<?php
-
 /*
  * This file is part of ibrand/EC-Open-Server.
  *
@@ -8,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace iBrand\EC\Open\Server\Http\Controllers;
 
 use iBrand\Component\Advert\Repositories\AdvertItemRepository;
